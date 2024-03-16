018c79d4929a78e6471b985ea014ea7b11ebe7fd
SON-LE\xxx <son.lequang97@gmail.com>
2024-03-16 20:51:16 +0700

    change code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import React, { useEffect } from "react";
+import React from "react";
 import GroupList from "./components/group/GroupList";
-import { getListGroupAsyncAction } from "./redux/reducers/groupSlide";
-import {
-  selectListGroups,
-  selectLoading,
-} from "./redux/selectors/groupSelector";
-import WithLoading from "./hoc/withLoading";
-import { connect } from "react-redux";
+
+// import WithLoading from "./hoc/withLoading";
 import Home from "./components/Home";
 import { Switch, Route, Link, Redirect } from "react-router-dom";
 
 function App(props) {
-  const getListGroup = props.getListGroupAsyncAction;
+  // const getListGroup = props.getListGroupAsyncAction;
 
-  useEffect(() => {
-    getListGroup();
-  }, [getListGroup]);
+  // useEffect(() => {
+  //   getListGroup();
+  // }, [getListGroup]);
 
-  const GroupListWithLoading = WithLoading(GroupList);
+  // const GroupListWithLoading = WithLoading(GroupList);
 
   return (
     <div>
@@ -34,27 +29,10 @@ function App(props) {
 
       <Switch>
         <Route path="/" component={Home} exact />
-        <Route
-          path="/groups"
-          component={() => (
-            <GroupListWithLoading
-              users={props.users}
-              isLoading={props.isLoading}
-            />
-          )}
-          exact
-        />
+        <Route path="/groups" component={GroupList} exact />
         <Redirect to="/" />
       </Switch>
     </div>
   );
 }
-
-const mapGlobalStateToProps = (state) => {
-  return {
-    users: selectListGroups(state),
-    isLoading: selectLoading(state),
-  };
-};
-
-export default connect(mapGlobalStateToProps, { getListGroupAsyncAction })(App);
+export default App;
